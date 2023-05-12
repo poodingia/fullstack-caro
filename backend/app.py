f02f3fe1b35c61dd5213ae98a42b7c48261838df
dkhien <dkhien.work@gmail.com>
2023-05-12 14:27:47 +0700

    stop sending move when game over BE

--- backend/app.py
+++ backend/app.py
@@ -66,7 +66,7 @@ class GameClient:
                 self.room_id = data.get("room_id")
 
             # Nhận thông tin trò chơi
-            elif data.get("board"):
+            elif data.get("board") and data.get("status")==None:
                 # Nếu là lượt đi của đội của mình thì gửi nước đi             
                 log_game_info(game_info=game_info)
                 if data.get("turn") in self.team_id:
@@ -86,7 +86,7 @@ class GameClient:
                         print("Invalid move")
 
             # Kết thúc trò chơi
-            elif data.get("status"):
+            elif data.get("status")!=None:
                 print("Game over")
                 break
 
